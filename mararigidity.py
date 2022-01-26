274f44c56ff6117674b803ac730956762ac66ef2
Yottaphy <jorge13romero@gmail.com>
2022-01-26 17:53:11 +0200

    fixed lookup bug

--- mararigidity.py
+++ mararigidity.py
@@ -29,6 +29,11 @@ def getElement(a,z):
     symbol = elements[z] if z < 119 else 'X'
     return n, symbol
 
+def getNumber(query):
+    for z, symbol in elements.items():
+        if symbol == query: return z
+    return 'Error'
+
 elements = {0:"n", 1:"H", 2:"He", 3:"Li", 4:"Be", 5:"B", 6:"C", 7:"N", 8:"O", 9:"F", 10:"Ne", 11:"Na", 12:"Mg", 13:"Al", 14:"Si", 15:"P", 16:"S", 17:"Cl", 18:"Ar", 19:"K", 20:"Ca", 21:"Sc", 22:"Ti", 23:"V", 24:"Cr", 25:"Mn", 26:"Fe", 27:"Co", 28:"Ni", 29:"Cu", 30:"Zn", 31:"Ga", 32:"Ge", 33:"As", 34:"Se", 35:"Br", 36:"Kr", 37:"Rb", 38:"Sr", 39:"Y", 40:"Zr", 41:"Nb", 42:"Mo", 43:"Tc", 44:"Ru", 45:"Rh", 46:"Pd", 47:"Ag", 48:"Cd", 49:"In", 50:"Sn", 51:"Sb", 52:"Te", 53:"I", 54:"Xe", 55:"Cs", 56:"Ba", 57:"La", 58:"Ce", 59:"Pr", 60:"Nd", 61:"Pm", 62:"Sm", 63:"Eu", 64:"Gd", 65:"Tb", 66:"Dy", 67:"Ho", 68:"Er", 69:"Tm", 70:"Yb", 71:"Lu", 72:"Hf", 73:"Ta", 74:"W", 75:"Re", 76:"Os", 77:"Ir", 78:"Pt", 79:"Au", 80:"Hg", 81:"Tl", 82:"Pb", 83:"Bi", 84:"Po", 85:"At", 86:"Rn", 87:"Fr", 88:"Ra", 89:"Ac", 90:"Th", 91:"Pa", 92:"U", 93:"Np", 94:"Pu", 95:"Am", 96:"Cm", 97:"Bk", 98:"Cf", 99:"Es", 100:"Fm", 101:"Md", 102:"No", 103:"Lr", 104:"Rf", 105:"Db", 106:"Sg", 107:"Bh", 108:"Hs", 109:"Mt", 110:"Ds", 111:"Rg", 112:"Cn", 113:"Nh", 114:"Fl", 115:"Mc", 116:"Lv", 117:"Ts", 118:"Og"}
 window = tk.Tk()
 window.configure(bg='#ededed')
@@ -63,8 +68,8 @@ def namer(event):
     if symb == 'Fy':
         lbl_Z['text'] = 137
         return 0
-    lbl_Z['text'] = elements.index(symb) if symb in elements else 'Error'
-
+    lbl_Z['text'] = getNumber(symb)
+ 
 frm_radius0 = tk.Frame(master = window, relief=tk.RAISED, borderwidth=1)
 frm_radius0.grid(row=0,column=1, columnspan= 1, padx=5, pady=5, sticky = 'nsew')
 lbl_radius0_name = tk.Label(master=frm_radius0, text="r₀", width=10, font='Helvetica 12 bold')
@@ -74,8 +79,6 @@ ent_radius0.pack(side=tk.LEFT)
 ent_radius0.insert(0, '1.4')
 lbl_radius0_unit = tk.Label(master=frm_radius0, text="fm", width=3)
 lbl_radius0_unit.pack(side=tk.LEFT)
-lbl_radius0_formula = tk.Label(master=frm_radius0, text="r = r₀ A"+superscript[1]+superscript[3], width=3)
-lbl_radius0_formula.pack(side=tk.LEFT)
 
 frm_proj = tk.Frame(master = window, relief=tk.RAISED, borderwidth=1)
 frm_proj.grid(row=1,column=0, padx=5, pady=5, sticky = 'nsew')
